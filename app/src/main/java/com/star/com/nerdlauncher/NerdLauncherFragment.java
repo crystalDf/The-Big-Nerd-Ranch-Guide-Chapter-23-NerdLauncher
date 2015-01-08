27899f3663c42609b2eec0e3de6be87c78e8dbe7
crystalDf <chendong333@gmail.com>
2015-01-08 15:18:28 +0800

    Chapter 23\NerdLauncher finished

--- app/src/main/java/com/star/com/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/star/com/nerdlauncher/NerdLauncherFragment.java
@@ -2,6 +2,7 @@ package com.star.com.nerdlauncher;
 
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.Collections;
@@ -50,15 +52,28 @@ public class NerdLauncherFragment extends ListFragment {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 PackageManager packageManager = getActivity().getPackageManager();
-                View v = super.getView(position, convertView, parent);
+                View view = super.getView(position, convertView, parent);
 
-                TextView textView = (TextView) v;
+                TextView textView = (TextView) view;
                 ResolveInfo resolveInfo = getItem(position);
                 textView.setText(resolveInfo.loadLabel(packageManager));
-                return v;
+                return view;
             }
         };
 
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        ResolveInfo resolveInfo = (ResolveInfo) l.getAdapter().getItem(position);
+        ActivityInfo activityInfo = resolveInfo.activityInfo;
+
+        if (activityInfo != null) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setClassName(activityInfo.applicationInfo.packageName, activityInfo.name);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
     }
 }
